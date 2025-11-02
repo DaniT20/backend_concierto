11a9fa23c2e736a32d4e05b971426a53529d7969
Daniel Telenchana <dani20telenchana@hotmail.com>
2025-11-02 11:14:18 -0500

    port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // CORS si lo necesitas
+  app.enableCors({ origin: true });
+
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  // En Render usa 0.0.0.0 para exponer correctamente
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Server listening on http://0.0.0.0:${port}`);
 }
 bootstrap();
